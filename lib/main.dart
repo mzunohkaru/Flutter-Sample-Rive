f7ca971913b266586e99e42dd460b401e47e7d24
mzunohkaru <winkaisyu@gmail.com>
2024-08-24 21:00:48 +0900

    add:default animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rive/rive.dart';
 
@@ -6,26 +7,40 @@ void main() => runApp(MaterialApp(
       home: MyRiveAnimation(),
     ));
 
-class MyRiveAnimation extends StatelessWidget {
+class MyRiveAnimation extends StatefulWidget {
   MyRiveAnimation({super.key});
 
-  /// いいね
-  SMIInput<bool>? _isMouseClick;
-  SMIInput<bool>? _isMouseOut;
-  SMIInput<bool>? _isMouseOver;
+  @override
+  State<MyRiveAnimation> createState() => _MyRiveAnimationState();
+}
 
-  /// RiveAnimationの初期化 Widget build時に呼ばれる
+class _MyRiveAnimationState extends State<MyRiveAnimation> {
+  // デフォルト
   void _onRiveInit(Artboard artboard) {
     final controller = StateMachineController.fromArtboard(
       artboard,
-      'State Machine 1', // RiveEditorの(Animations欄) StateMachine名
+      'State Machine',
     );
     artboard.addController(controller!);
-    // RiveEditorの(Inputs欄) ※Attention:初期値はnullなので注意が必要
-    _isMouseClick = controller.findInput<bool>('mouse-click') as SMIBool;
-    _isMouseOut = controller.findInput<bool>('mouse-out') as SMIBool;
-    _isMouseOver = controller.findInput<bool>('mouse-over') as SMIBool;
   }
+
+  /// いいね
+  // SMIInput<bool>? _isMouseClick;
+  // SMIInput<bool>? _isMouseOut;
+  // SMIInput<bool>? _isMouseOver;
+
+  /// RiveAnimationの初期化 Widget build時に呼ばれる
+  // void _onRiveInit(Artboard artboard) {
+  //   final controller = StateMachineController.fromArtboard(
+  //     artboard,
+  //     'State Machine 1', // RiveEditorの(Animations欄) StateMachine名
+  //   );
+  //   artboard.addController(controller!);
+  //   // RiveEditorの(Inputs欄) ※Attention:初期値はnullなので注意が必要
+  //   _isMouseClick = controller.findInput<bool>('mouse-click') as SMIBool;
+  //   _isMouseOut = controller.findInput<bool>('mouse-out') as SMIBool;
+  //   _isMouseOver = controller.findInput<bool>('mouse-over') as SMIBool;
+  // }
 
   /// ホバー
   // SMIInput<bool>? _isHover;
@@ -42,24 +57,29 @@ class MyRiveAnimation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // いいね
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: GestureDetector(
-            onTap: () {
-              _isMouseClick?.value = true;
-              print("DEBUG: Good");
-            },
-            child: RiveAnimation.asset(
-              'assets/Twitter_Like_with_Audio.riv',
-              fit: BoxFit.cover,
-              onInit: _onRiveInit,
-              stateMachines: ['State Machine 1'],
-            ),
-          ),
+        // デフォルト
+        child: RiveAnimation.asset(
+          'assets/untitled.riv',
         ),
-        // ホバー
+
+        /// いいね
+        // child: SizedBox(
+        //   width: 100,
+        //   height: 100,
+        //   child: GestureDetector(
+        //     onTap: () {
+        //       _isMouseClick?.value = true;
+        //       print("DEBUG: Good");
+        //     },
+        //     child: RiveAnimation.asset(
+        //       'assets/Twitter_Like_with_Audio.riv',
+        //       fit: BoxFit.cover,
+        //       onInit: _onRiveInit,
+        //       stateMachines: ['State Machine 1'],
+        //     ),
+        //   ),
+        // ),
+        /// ホバー
         // child: Stack(
         //   children: [
         //     RiveAnimation.asset(
